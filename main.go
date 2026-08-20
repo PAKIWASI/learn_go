bb0d9a1e8993cf8272c4e56e4813ef4e0a0e4694
PAKIWASI <ullahwasi26@gmail.com>
2026-08-20 13:25:38 +0500

    fix

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	// gameoflife "github.com/PAKIWASI/learn_go/game_of_life"
-	"github.com/PAKIWASI/learn_go/concurrency"
+	// concurrency "github.com/PAKIWASI/learn_go/concurrency"
 	// httpserver "github.com/PAKIWASI/learn_go/http_server"
 )
 
@@ -12,6 +12,7 @@ import (
 func main() {
 
 	// gameoflife.Run()
-	concurrency.Run()
+	// concurrency.Run()
 	// httpserver.Run()
+
 }
